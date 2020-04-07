ddebb4663716443ef5da5ff0b26f70ecef3d8d46
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-07 19:25:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,4 @@
 class Dog
-  def name=(dog_name)
-    @name = dog_name
-  end
-  def name
-    @name
-  end
-  def breed=(dog_breed)
-    @breed = dog_breed
-  end
-  def breed
-    @breed
-  end
+  attr_accessor :name
+  attr_accessor :breed
 end
